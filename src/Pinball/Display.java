c8ce2dac30488127a3c374eef7113ba469d924dd
Charles <chabermehl@unm.edu>
2018-08-26 23:22:14 -0600

    added given functions to display class

--- src/Pinball/Display.java
+++ src/Pinball/Display.java
@@ -1,4 +1,52 @@
 package Pinball;
 
 public class Display {
+
+    private int boardWidth;
+    private int boardHeight;
+    private int boardRows;
+    private int boardColumns;
+
+    public Display (int boardWidth, int boardHeight, int boardRows, int boardColumns) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        this.boardRows = boardRows;
+        this.boardColumns = boardColumns;
+    }
+
+    public int getBoardWidth() {
+        return this.boardWidth;
+    }
+
+    public int getBoardHeight() {
+        return this.boardHeight;
+    }
+
+    public int getBoardRows() {
+        return this.boardRows;
+    }
+
+    public int getBoardColumns() {
+        return this.boardColumns;
+    }
+
+    public void setBallAt() {
+
+    }
+
+    public void setTileColor(int row, int col, boolean state) {
+
+    }
+
+    public void setPlayButton(boolean state) {
+
+    }
+
+    public void setResetButton(boolean state) {
+
+    }
+
+    public void setScoreValue(int value) {
+
+    }
 }
