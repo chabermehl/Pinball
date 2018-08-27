fcb0ab8b41ee791a112510c0339e1468c6a30ab2
Charles <chabermehl@unm.edu>
2018-08-26 23:26:06 -0600

    initialized score class

--- src/Pinball/Score.java
+++ src/Pinball/Score.java
@@ -1,4 +1,19 @@
 package Pinball;
 
 public class Score {
+    private int score;
+    public Score (int score){
+        this.score = score;
+    }
+    public void reset() {
+        this.score = 0;
+    }
+
+    public int getCurrentValue() {
+        return this.score;
+    }
+
+    public void incrementBy(int value) {
+        this.score += value;
+    }
 }
