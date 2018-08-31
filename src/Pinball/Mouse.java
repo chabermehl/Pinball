6fbc20be5797560a312adba510fcb9e48dd5d8fc
Charles Habermehl <chabermehl@cs.unm.edu>
2018-08-31 05:03:21 -0600

    its so late...

--- src/Pinball/Mouse.java
+++ src/Pinball/Mouse.java
@@ -1,3 +1,12 @@
+/*
+ * @author Charles Habermehl
+ * CS 351
+ * 8/31/18
+ * Pinball
+ */
+
+//really wish i had time to use this class
+
 package Pinball;
 
 import javafx.event.EventHandler;
@@ -6,12 +15,22 @@ import javafx.scene.input.MouseEvent;
 public class Mouse {
     private double x;
 
+    /**
+     * captures where the mouse clicks
+     *
+     * @return x coord of mouse click
+     */
     public EventHandler<MouseEvent> getMouseMovedHandler() {
         return event -> {
             x = event.getX();
         };
     }
 
+    /**
+     * getter for x click
+     *
+     * @return double value
+     */
     public double getX() {
         return x;
     }
