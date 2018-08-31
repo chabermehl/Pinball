64ace5074f664a718900e72b56c8df38912808d5
Charles Habermehl <chabermehl@cs.unm.edu>
2018-08-31 05:02:51 -0600

    git is great til it isnt

--- src/Pinball/Display.java
+++ src/Pinball/Display.java
@@ -1,3 +1,9 @@
+/*
+ * @author Charles Habermehl
+ * CS 351
+ * 8/31/18
+ * Pinball
+ */
 package Pinball;
 
 public class Display {
@@ -7,25 +13,53 @@ public class Display {
     private int boardRows;
     private int boardColumns;
 
-    public Display (int boardWidth, int boardHeight, int boardRows, int boardColumns) {
+    /**
+     * constructor htat hold board size
+     *
+     * @param boardWidth   pixel width of board
+     * @param boardHeight  pixel height of board
+     * @param boardRows    number of rows in board
+     * @param boardColumns number of columns in board
+     */
+    public Display(int boardWidth, int boardHeight, int boardRows, int boardColumns) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         this.boardRows = boardRows;
         this.boardColumns = boardColumns;
     }
 
+    /**
+     * gets board width
+     *
+     * @return integer value
+     */
     public int getBoardWidth() {
         return this.boardWidth;
     }
 
+    /**
+     * gets board height
+     *
+     * @return integer value
+     */
     public int getBoardHeight() {
         return this.boardHeight;
     }
 
+    /**
+     * gets number of rows the board has
+     *
+     * @return integer value
+     */
     public int getBoardRows() {
         return this.boardRows;
     }
 
+    /**
+     * returns number of columns the board has
+     *
+     * @return integer value
+     */
     public int getBoardColumns() {
         return this.boardColumns;
     }
