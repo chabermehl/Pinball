8d002bacc5e99c6ff90b8e7916d0a1d15f73208e
Charles <chabermehl@unm.edu>
2018-08-31 01:11:04 -0600

    making encapsulated mouse class

--- src/Pinball/Mouse.java
+++ src/Pinball/Mouse.java
@@ -1,4 +1,19 @@
 package Pinball;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+
 public class Mouse {
+    private double x;
+
+    public EventHandler<MouseEvent> getMouseMovedHandler() {
+        return event -> {
+            x = event.getX();
+        };
+    }
+
+    public double getX() {
+        return x;
+    }
+
 }
