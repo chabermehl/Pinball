0e7dd52a0c121f7329fe33b9ad354dfec662f545
Charles Habermehl <chabermehl@cs.unm.edu>
2018-08-31 05:02:22 -0600

    boo git sucks

--- src/Pinball/Tile.java
+++ src/Pinball/Tile.java
@@ -1,17 +1,38 @@
+/*
+ * @author Charles Habermehl
+ * CS 351
+ * 8/31/18
+ * Pinball
+ */
 package Pinball;
 
 public class Tile {
 
     private boolean state;
 
+    /**
+     * tile constructor
+     *
+     * @param state takes a boolean for the state of the tile
+     */
     public Tile(boolean state) {
         this.state = state;
     }
 
+    /**
+     * gets the state object
+     *
+     * @return state object
+     */
     public boolean getState() {
         return this.state;
     }
 
+    /**
+     * set the state of the tile
+     *
+     * @param isPoint boolean value
+     */
     public void setState(boolean isPoint) {
         this.state = isPoint;
     }
