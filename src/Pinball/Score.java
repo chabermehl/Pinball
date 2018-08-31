ee0dc3e9b8fd4f5c95d90ee99f967c219cf273e7
Charles Habermehl <chabermehl@cs.unm.edu>
2018-08-31 05:01:45 -0600

    git wont push :(

--- src/Pinball/Score.java
+++ src/Pinball/Score.java
@@ -1,17 +1,39 @@
+/*
+ * @author Charles Habermehl
+ * CS 351
+ * 8/31/18
+ * Pinball
+ */
 package Pinball;
 
 public class Score {
 
     private int score;
 
-    public Score (int score){
+    /**
+     * score constructor
+     *
+     * @param score integer value of the score
+     */
+    public Score(int score) {
         this.score = score;
     }
 
+    /**
+     * gets the current score
+     *
+     * @return score object
+     */
     public int getCurrentValue() {
         return this.score;
     }
 
+    /**
+     * increments the score by a chosen value
+     *
+     * @param value integer value
+     * @return score object incremented by the integer value
+     */
     public int incrementBy(int value) {
         return this.score += value;
     }
